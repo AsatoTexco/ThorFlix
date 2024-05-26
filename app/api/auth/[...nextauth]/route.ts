dcb2329d6276ddc198b77baf2f27caaa0100b431
Arthur Simões <arthur2006.teixeira@gmail.com>
2024-05-26 12:53:40 -0300

    fix bug deploy

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,12 @@ import FacebookProvider from "next-auth/providers/facebook";
 import { authOptions } from '../../../../config/authOptions';
  
 export const handler = NextAuth(authOptions);
-export {handler as GET, handler as POST}
+ 
+
+export const GET = (req, res) => {
+  return handler(req, res);
+}
+
+export const POST = (req, res) => {
+  return handler(req, res);
+}

10ad0ee745d10262f02b9ae6276031ea017b9b94
Arthur Simões <arthur2006.teixeira@gmail.com>
2024-05-26 12:30:27 -0300

    middleware teste deploy

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,23 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server' 
-import {  decodeJwt } from 'jose';    
+import {  decodeJwt } from 'jose';     
 
-import { getCsrfToken } from 'next-auth/react';
- 
  
 // MIDDLEWARE VALIDAR LOGIN  
 export async function middleware(request: NextRequest) {
   
-  const csrfToken = await getCsrfToken()
-  
- 
+  var cookie_token = request.cookies.get("token") 
+  var token = ''   
+
+  if(typeof cookie_token != "undefined"){
+    token = cookie_token.value 
+    if(!decodeJwt(token)['id']){
+      return NextResponse.redirect(new URL('/login', request.url)); 
+    } 
+    // LOGADO 
+  } else{
+    return NextResponse.redirect(new URL('/login', request.url)); 
+  }
 }
 
 // PATHS 

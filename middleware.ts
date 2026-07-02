340d91adc65c1b965f5d34b448ea81ee7a8b3d8f
Arthur <arthur2006.teixeira@gmail.com>
2026-07-02 17:09:16 -0300

    fix again

--- middleware.ts
+++ middleware.ts
@@ -19,12 +19,7 @@ function getCookieValue(cookieString, cookieName) {
             return cookie.substring(cookieName.length + 1);
         }
     } 
-
-
-
-    
   }
-
 
   // se o cookie não for encontrado
   return null;
